433fda8b29992d857ae00f7244e2e65af080a73e
Danica Tiegs <clear>
2021-01-09 19:17:39 -0800

    completed code

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,3 +1,6 @@
+//table is populating without errors, but there is a large gap at the top
+//button is not working
+
 // from data.js
 var tableData = data;
 
@@ -8,77 +11,57 @@ var tbody = d3.select("tbody");
 // Console.log the ufo sighting data from data.js
 console.log(tableData);
 
-// Step 1: Loop Through `tableData` and console.log each tableData object
-tableData.forEach(function(sighting) {
-  console.log(sighting);
-});
-
-// Step 2:  Use `Object.entries` to console.log each ufo sighting value
-tableData.forEach(function(sighting) {
-  console.log(tableData);
-
-  Object.entries(tableData).forEach(function([key, value]) {
-    console.log(key, value);
-  });
-});
-
-// Step 3:  Use d3 to append one table row `tr` for each sighting object
-
-tableData.forEach(function(sighting) {
-  console.log(sighting);
-Object.entries(sighting).forEach(function([key, value]) {
-  console.log(key, value);
-});
-  var row = tbody.append("tr");
-});
-
-// Step 4: Use d3 to append 1 cell per sighting value
-tableData.forEach(function(sighting) {
-  console.log(sighting);
-  var row = tbody.append("tr");
-
-  Object.entries(sighting).forEach(function([key, value]) {
-    console.log(key, value);
-    // Append a cell to the row for each value in sighting data object
-  
-    var cell = row.append("td");
-  });
-});
 
 // Step 5: Use d3 to update each cell's text with ufo sighting data
 
-data.forEach(function(tableData) {
-  console.log(tableData);
+tableData.forEach(function(sighting) {
+  console.log(sighting);
   var row = tbody.append("tr");
-  Object.entries(tableData).forEach(function([key, value]) {
+  Object.entries(sighting).forEach(function([key, value]) {
     console.log(key, value);
-    // Append a cell to the row for each value
-    // in the weather report object
+
+    // Append a cell to the row for each value in tableData object
     var cell = row.append("td");
     cell.text(value);
   });
 });
 
 // Select the button
-var button = d3.select("#button");
+var button = d3.select("#filter-btn");
 
 // Select the form
-var input = d3.select("#form");
-
-
-//Select input element
-var inputElement = d3.select("#example-form-input");
-
-var inputValue = inputElement.date("value");
-
-console.log(inputValue);
+var form = d3.select("#form");
 
 // Create event handlers 
 button.on("click", runEnter);
 form.on("submit", runEnter);
 
+// Complete the event handler function for the form
+function runEnter() {
+console.log("run")
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+  
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime").property("value");
 
-data.forEach(sighting => {
+  console.log(inputElement);
+
+  var inputData = tableData.filter(sighting => sighting.datetime === inputElement);
+
+  console.log(inputData);
+  tbody.html("")
+
+  inputData.forEach(function(sighting) {
     console.log(sighting);
-console.log('')
-})
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key, value]) {
+      console.log(key, value);
+  
+      // Append a cell to the row for each value in tableData object
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+
+}
